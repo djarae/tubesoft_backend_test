7e6fad331d0cbdddee0ce591827a96f0db914127
djarae <djaraestivill@gmail.com>
2021-05-18 20:08:09 -0400

    14 finally works the interaction api and front

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
 
 // define a simple route
 app.get('/', function(req, res){
   res.json({"message": "WELCOME ."});
 });
 
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin: *" )
-}) 
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin: *" )
+// }) 
 
 require('./routes/chronometer.routes')(app);
 console.log("funciona el require dontes ")
 
-app.listen(8070, () => console.log('server listening on port 8080'));
+app.listen(8070, () => console.log('server listening on port 8070'));
